ca72886b3ca2167dd5fb17826ca04c755b262aee
mateo2208 <mateoandres2208@gmail.com>
2025-05-06 21:47:02 -0400

    Cambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-export default function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Navbar from "./components/Navbar";
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-} 
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
